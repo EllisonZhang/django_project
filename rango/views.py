b8195faf6611810fcb6fdf9ebe8d90b4f2955c4d
Ellison Zhang <2358826z@student.gla.ac.uk>
2019-01-25 14:08:36 +0000

    chapter4 finished

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    context_dict = {'boldmessage':"Eat it!"}
+    return render(request, 'rango/about.html', context = context_dict)
