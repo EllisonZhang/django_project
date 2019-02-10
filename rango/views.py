6abe91482608b63d09fdc4909e420bb9afe16bf8
Ellison Zhang <2358826z@student.gla.ac.uk>
2019-02-10 19:46:03 +0000

    chapter10 finished

--- rango/views.py
+++ rango/views.py
@@ -18,7 +18,7 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list =Page.objects.order_by('views')[:5]
     context_dict = {'categories': category_list,'pages':page_list}
-    
+
     # Call the helper function to handle the cookies
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
@@ -143,9 +143,12 @@ def about(request):
     if request.session.test_cookie_worked(): 
         print("TEST COOKIE WORKED!") 
         request.session.delete_test_cookie()
-
+    visitor_cookie_handler(request)  
     context_dict = {'boldmessage':"Eat it!"}
-    return render(request, 'rango/about.html', context = context_dict)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/about.html', context = context_dict)
+
+    return response
 
 def register(request):
     # A boolean value for telling the template
